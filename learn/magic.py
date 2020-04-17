f69cd0458b33996b4a95db41c64f4eb2372e5796
nhutphong <nhutphong@outlook.com>
2020-04-17 12:11:03 +0700

    17-4

--- learn/magic.py
+++ learn/magic.py
@@ -1,6 +1,6 @@
 """
     DATA MODEL, BASIC CUTOMIZATION
-    Special method
+    Magic method
 
 """
 __IMPORTANT__="""
@@ -22,7 +22,7 @@ __IMPORTANT__="""
 
     self, super().func(no self) => class()=object.func(have a self)
     value = int, str
-    khi tao cac special method neu gap cac syntax trong code or ngay return:
+    khi tao cac Magic method neu gap cac syntax trong code or ngay return:
     self + value thi se call them __add__
     value + self thi se call them __radd__
 
@@ -32,21 +32,34 @@ __IMPORTANT__="""
     comparison: <,<=,==,!=, ...,
 
 
-    khi gặp: self +-*/ other=self -> run special methods operators tương ứng
-    value+self -> run special methods operators right
+    khi gặp: self +-*/ other=self -> run Magic methods operators tương ứng
+    value+self -> run Magic methods operators right
     len(self) * value > run __len__(self) return giá trị cụ thể(int, str)=value rồi nhân * với value
 
     nói chung: value +value cộng bình thường
 
-    self+value, value < self, self + self sẽ run các special methods operators or comparisons tương ứng,
+    self+value, value < self, self + self sẽ run các Magic methods operators or comparisons tương ứng,
 
-    và còn những methods khác len(self), str(self), blabla, ... các built-in functions đều có special methods nếu ta định nghĩa nó khi tạo class, để run nó thì code func_name(self) trong các methods
+    và còn những methods khác len(self), str(self), blabla, ... các built-in functions đều có Magic methods nếu ta định nghĩa nó khi tạo class, để run nó thì code func_name(self) trong các methods
 
 code self[pos] -> run __getitem__(self, pos)
 """
 
-class Special:
-    """Tao la Special class"""
+def design(func_name=None, letter='#'):
+    def function(func):
+        def wrapped(*args, **kwargs):
+            print(f"{func_name+ ' START ':{letter}^85}")
+            fun = func(*args, **kwargs)
+            print(f"{func_name+ ' END ':{letter}^85}", end='\n'*2)
+            return fun
+
+        return wrapped
+        
+    return function
+
+
+class Magic:
+    """Tao la Magic class"""
     __number = 0
     __string = ''
     count_init = 0
@@ -58,110 +71,127 @@ class Special:
     # chỉ có thể dùng publish method của parrent_class để truy cập
     # neu la class variable publish thi access binh thuong child_class.count_init, child_class.count_getattr, ...
 
+    @design("__init__")
     def __init__(self, *, number=10, intdf=5, strdf='phong', listdf=[]):
-        Special.count_init += 1
-        print(f"Tao la __init__ {Special.count_init}")
-        self.__len = number # _Special__len
-        self.__str = strdf  # _Special__str
-        self.__list = listdf # _Special__list
-        self.__int = intdf # _Special__int
-        # child_class có thể access self._Special__int, syntax đặc biệt
+        Magic.count_init += 1
+        print(f"Tao la __init__ {Magic.count_init}")
+        self.__len = number # _Magic__len
+        self.__str = strdf  # _Magic__str
+        self.__list = listdf # _Magic__list
+        self.__int = intdf # _Magic__int
+        # child_class có thể access self._Magic__int, syntax đặc biệt
         #
 
 
     # khi len(self) => run:  __len__(self)
+    @design("__len__")
     def __len__(self):
         print('__len__')
         return self.__len  # self__len = int (bat buoc)
     # khi print(obj) or str(obj) or obj.__str__() => run __str__(self) = self.__str
-
+    @design("__str__")
     def __str__(self):
-        Special.count_str += 1
-        print(f"__str__ {Special.count_str}")
+        Magic.count_str += 1
+        print(f"__str__ {Magic.count_str}")
         return self.__str  # self__str = str bat buoc
 
+    @design("__repr__")
     def __repr__(self):
         print("__repr__")
         return f"<enter> run __repr__ = {self.__str}"  # dung repr(obj)
 
-    def __setitem__(self, attr, value):
+    @design("__setitem__")
+    def __setitem__(self, key, value):
         print('__setitem__')
-        # self[attr] = value
-        self.__dict__[attr] = value
+        # self[key] = value
+        self.__dict__[key] = value
 
-    def __getitem__(self, attr):
+    @design("__getitem__")
+    def __getitem__(self, key):
         print("__getitem__")
         # self[::], self[index] #RecursionError
-        return self.__dict__[attr]
+        return self.__dict__[key]
 
-    def __delitem__(self, attr):
+    @design("__delitem__")
+    def __delitem__(self, key):
+        # del self[key]
         print('__delitem__')
-        del self.__dict__[attr]
+        del self.__dict__[key]
 
+    @design("__getattribute__")
     def __getattribute__(self, attr): #3
-        Special.count_getattribute += 1
-        print(f'<{attr}> ton tai nen run __getattribute__ {Special.count_getattribute}')
+        # self.attr ton tai
+        Magic.count_getattribute += 1
+        print(f'<{attr}> ton tai nen run __getattribute__ {Magic.count_getattribute}')
         # super().__getattribute__(attr) # run __getattr__(self, attr) #2
         # object.__getattribute__(self,attr) #2
         return object.__getattribute__(self,attr)
         #super() | object.__getattribute__(self, attr) return VALUE neu self.attr ton tai, else run __getattr(self, attr) 1 lan duy nhat
         #
+
+    @design("__getattr__")
     def __getattr__(self, attr): #2
-        Special.count_getattr += 1
-        print(f'<{attr}> NONO ton tai nen run __getattr__ {Special.count_getattr} <{attr}>')
+        # self.old -> run 1 lan khi self.old khong ton tai
+        Magic.count_getattr += 1
+        print(f'<{attr}> NONO ton tai nen run __getattr__ {Magic.count_getattr} <{attr}>')
 
         value = 'invalid'
         setattr(self, attr, value)
         return value
 
+    @design("__setattr__")
     def __setattr__(self, attr, value): #1
         # khi tao obj neu co 4 attr se run 4 lan __setattr__
         self.__dict__[attr] = value
 
-        Special.count_setattr += 1
-        print(f"Tao la __setattr__ {Special.count_setattr}")
+        Magic.count_setattr += 1
+        print(f"Tao la __setattr__ {Magic.count_setattr}")
         if not isinstance(value, list):
             print(f"<{attr}> {value:.>20}", end='\n'*2)
         else:
             print(f"<{attr}> {'':.<20}{value}", end='\n'*2)
 
-
+    @design("__delattr__")
     def __delattr__(self, attr):
+        # del self.attr
         print('__delattr__')
+        del self.__dict__[attr]
 
+    @design("__del__")
     def __del__(self):
+        #del self
         print("__del__")
 
     def __add__(self, other):
         print('tao la __add__+')
 
-        return Special(intdf=self.__int + other.__int)
+        return Magic(intdf=self.__int + other.__int)
         # return self.__str + other.__str
 
     def __sub__(self, other):
         print('tao la __sub__-')
 
-        return Special(intdf=self.__int - other.__int)
+        return Magic(intdf=self.__int - other.__int)
 
     def __mul__(self, other):
         print('tao la __mul__*')
 
-        return Special(intdf=self.__int * other.__int)
+        return Magic(intdf=self.__int * other.__int)
 
     def __truediv__(self, other):
         print('tao la __truediv__/')
 
-        return Special(intdf=self.__int / other.__int)
+        return Magic(intdf=self.__int / other.__int)
 
     def __floordiv__(self, other):
         print('tao la __floordiv__//')
 
-        return Special(intdf=self.__int // other.__int)
+        return Magic(intdf=self.__int // other.__int)
 
     def __mod__(self, other):
         print('tao la __mod__%')
 
-        return Special(intdf=self.__int % other.__int)
+        return Magic(intdf=self.__int % other.__int)
 
     def __divmod__(self, other):
         print('__divmod__ (nguyen, du)')
@@ -221,7 +251,7 @@ class Special:
         if isinstance(value, int):
             self.__len = value*2
         else:
-            self.__len = Special.__number
+            self.__len = Magic.__number
 
     @property
     def _str(self):
@@ -232,11 +262,11 @@ class Special:
         if isinstance(value, str):
             self.__str = value.upper()
         else:
-            self.__str = Special.__string
+            self.__str = Magic.__string
 
 
-class ChildSpecial(Special):
-    """docstring for ChildSpecial"""
+class ChildMagic(Magic):
+    """docstring for ChildMagic"""
     def __init__(self):
         super().__init__()
 
@@ -247,12 +277,12 @@ class ChildSpecial(Special):
         return len(self) # recursion vo tan call itself
 
 
-class Recursion(Special):
+class Recursion(Magic):
     """docstring for Recursion"""
     __IMPORTANT__ = """
         chỉ có thể dùng publish method đc thừa kế để access class variable private
         còn instance variable private có thẻ dùng syntas đặc biệt,
-        self._Special__int => nhưng nên dùng publish method
+        self._Magic__int => nhưng nên dùng publish method
     """
 
 
@@ -260,7 +290,7 @@ class Recursion(Special):
         super().__init__(intdf=intdf)
         self.full_name = full_name
         self.old = old
-        # child_class có thể access self._Special__int, syntax đặc biệt
+        # child_class có thể access self._Magic__int, syntax đặc biệt
         #
 
 
@@ -281,13 +311,13 @@ class Recursion(Special):
         setattr(self, attr, default_value)
         return default_value
 
-    def __getitem__(self, pos):
+    def __getitem__(self, key):
         print("__getitem__")
-        return self[pos] #RecursionError
+        return self[key] #RecursionError
 
-    def __setitem__(self, pos, value):
+    def __setitem__(self, key, value):
         print('__setitem__')
-        self[pos] = value #RecursionError
+        self[key] = value #RecursionError
 
     def __del__(self):
         print("__del__")
