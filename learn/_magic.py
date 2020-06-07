b65dd70f21bdcade7267b4d5a53e5d79c3c3eb1a
nhutphong <nhutphong@outlook.com>
2020-06-07 10:55:02 +0700

    7-6

--- learn/_magic.py
+++ learn/_magic.py
@@ -42,16 +42,16 @@ __IMPORTANT__="""
 """
 
 def design(func_name=None, letter='#'):
-    def function(func):
-        def wrapped(*args, **kwargs):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
             print(f"{func_name+ ' START ':{letter}^85}")
             fun = func(*args, **kwargs)
             print(f"{func_name+ ' END ':{letter}^85}", end='\n'*2)
             return fun
 
-        return wrapped
+        return wrapper
         
-    return function
+    return decorator
 
 
 class Magic:
